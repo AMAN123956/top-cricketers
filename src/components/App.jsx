615bc37eb32e963dbfe523371e19c54a6719ad3d
Your Name <you@example.com>
2020-07-24 23:26:02 +0530

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import collection from "../collection";
 import Card from "./Card";
 
+
 function createCard(term){
   return(  
-  <div>
+  
    
   <Card 
     key={term.id}
@@ -13,17 +14,30 @@ function createCard(term){
     name={term.name}
     img={term.img}
     url = {term.url}
+    odiMatches ={term.odi_matches}
+    odiRuns={term.odi_runs}
+    odiInn = {term.odi_inn}
+    testMatches ={term.test_matches}
+    testRuns={term.test_runs}
+    testInn = {term.test_inn}
+    t20Matches ={term.t20_matches}
+    t20Runs={term.t20_runs}
+    t20Inn = {term.t20_inn}
+    iplMatches ={term.ipl_matches}
+    iplRuns={term.ipl_runs}
+    iplInn = {term.ipl_inn}
    
     />
     
-    </div>);
+    );
 }
 
 function App() {
     return(<div>
         <h1 className="heading">List of Top 10 Batsman of the World</h1>
         {collection.map(createCard)}
-    
+        
+ 
     </div>);
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,62 @@
 import React from "react";
-
-
+import collection from "../collection";
+import {Table}  from "react-bootstrap";
 function Card(props) {
     return(
         <div className="card">
           <div class="card-detail">
-         <h1 className="index">{props.id}</h1>
+        <h1 className="index">{props.id} 
+        <span style={{color:"blue"}}>
+       
+        </span>
+        </h1> 
+             
+         
          <h2 className="name">{props.name}</h2>
          <img src={props.img} alt="Batsman"  className="profileImg"/> 
-        <a href={props.url} className="infoButton">
+        <a href={props.url} className="infoButton" target="_blank">
          <button className="btn btn-dark">More Info</button>
         </a>
+        <div className="stat">
+        <Table striped bordered hover variant="dark" responsive="md">
+  <thead>
+    <tr>
+      <th></th>
+      <th>M</th>
+      <th>Inn</th>
+      <th>Runs</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+      <td>Test</td>
+      <td>{props.testMatches}</td>
+      <td>{props.testInn}</td>
+      <td>{props.testRuns}</td>
+  </tr>
+    <tr>
+      <td>ODI</td>
+      <td>{props.odiMatches}</td>
+      <td>{props.odiInn}</td>
+      <td>{props.odiRuns}</td>
+    </tr>
+    <tr>
+      <td>T20</td>
+      <td>{props.t20Matches}</td>
+      <td>{props.t20Inn}</td>
+      <td>{props.t20Runs}</td>
+    </tr>
+    <tr>
+      <td>IPL</td>
+      <td>{props.iplMatches}</td>
+      <td>{props.iplInn}</td>
+      <td>{props.iplRuns}</td>
+    </tr>
+  </tbody>
+</Table>
+</div>
+      
+
         </div>
         </div>
     );
